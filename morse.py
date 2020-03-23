7c599562a78a8304b57ec1dab9ebfc652f42f0e1
Robert Sheehan <robertnsheehan@gmail.com>
2020-03-23 11:00:35 +0000

    started morse project

---
+++ morse.py
@@ -0,0 +1,11 @@
+# R. Sheehan 20 - 3 - 2020
+# Python script for converting input text into Morse code
+# Idea being to display Morse version of text using blinking LED
+# Down the line this could become a lab in which messages are transmitted
+# using Morse and received and decoded using a Pre-Amp Receiver
+# Will need to define a dictionary using the ITU Morse alphabet and
+# write a function for converting the text into Morse
+# Message will be transmitted using GPIO controlled blinking LED
+# Aim is to demonsrtate that something useful can be done with the RPi
+# Use as much of Python's built-in string handling capabilities as possible
+
